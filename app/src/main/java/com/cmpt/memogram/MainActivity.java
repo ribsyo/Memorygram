9366603edc849cd04fba40047230f3943d67a6f3
Michael Krishna <michaelkrishna@hotmail.com>
2024-11-01 06:45:31 +0000

    After authentication Stores user's name and groupID.

--- app/src/main/java/com/cmpt/memogram/MainActivity.java
+++ app/src/main/java/com/cmpt/memogram/MainActivity.java
@@ -1,15 +1,11 @@
 package com.cmpt.memogram;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,44 +13,28 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cmpt.memogram.databinding.ActivityMainBinding;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private ActivityMainBinding binding;
+    private String name;
+    private String groupID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mAuth = FirebaseAuth.getInstance();
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-    }
-    public void onStart() {
-        super.onStart();
-        mAuth.signInWithEmailAndPassword("test@test.ca", "PW12345")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("login", "loginWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("login", "loginUserWithEmail:failure", task.getException());
-                }
-            }
-        });;
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            setContentView(binding.getRoot());
-            reload();
-        }
-    }
-    public void reload() {
+        setContentView(binding.getRoot());
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -64,6 +44,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
+    public void onStart() {
+        super.onStart();
+        //Currently hardcoded to login with test user
+        mAuth.signInWithEmailAndPassword("test@test.ca", "PW12345")
+                .addOnCompleteListener(this, login -> {
+                    if (login.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("login", "loginWithEmail:success");
+
+                        FirebaseUser currentUser = mAuth.getCurrentUser();
+                        if(currentUser != null){
+                            String uID = currentUser.getUid();
+                            DocumentReference docRef = db.collection("Users").document(uID);
+                            docRef.get().addOnCompleteListener(getUser -> {
+                                if (getUser.isSuccessful()) {
+                                    DocumentSnapshot document = getUser.getResult();
+                                    if (document.exists()) {
+                                        Log.d("getUser", "DocumentSnapshot data: " + document.getData());
+                                        name = document.getString("name");
+                                        groupID = document.getString("groupID");
+                                        Log.d("getUser", groupID + " " + name);
+                                    } else {
+                                        Log.d("getUser", "No such document");
+                                    }
+                                } else {
+                                    Log.d("getUser", "get failed with ", getUser.getException());
+                                }
+                            });
+
+                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("login", "loginUserWithEmail:failure", login.getException());
+                    }
+                });
+    }
 }
