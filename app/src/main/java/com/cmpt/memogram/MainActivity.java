20a6ab1674a94f01f8c243f7facaad05560ef914
Michael Krishna <michaelkrishna@hotmail.com>
2024-11-01 05:36:48 +0000

    App now automatically logs in as test user

--- app/src/main/java/com/cmpt/memogram/MainActivity.java
+++ app/src/main/java/com/cmpt/memogram/MainActivity.java
@@ -1,9 +1,15 @@
 package com.cmpt.memogram;
 
-import android.os.Bundle;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,18 +17,44 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cmpt.memogram.databinding.ActivityMainBinding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mAuth = FirebaseAuth.getInstance();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+    }
+    public void onStart() {
+        super.onStart();
+        mAuth.signInWithEmailAndPassword("test@test.ca", "PW12345")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("login", "loginWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("login", "loginUserWithEmail:failure", task.getException());
+                }
+            }
+        });;
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            setContentView(binding.getRoot());
+            reload();
+        }
+    }
+    public void reload() {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
