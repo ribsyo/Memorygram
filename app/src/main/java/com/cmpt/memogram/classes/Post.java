0c5322e0245483d3cf72ce9ab85c69e62e825d75
caydeny <caydenyoo@hotmail.com>
2024-11-10 16:18:53 -0800

    merged with backend

---
+++ app/src/main/java/com/cmpt/memogram/classes/Post.java
@@ -0,0 +1,12 @@
+package com.cmpt.memogram.classes;
+
+public class Post {
+    public String title;
+    public String text;
+    public String audioPath;
+    public String localAudioPath;
+    public String imagePath;
+
+    public String localImagePath;
+    public Object datePosted;
+}
