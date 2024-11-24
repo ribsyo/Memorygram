64be13076b2cbba96241a0a1d326465055caa9cf
caydeny <caydenyoo@hotmail.com>
2024-11-24 13:19:13 -0800

    how to

---
+++ app/src/main/java/com/cmpt/memogram/ui/fragment/CollectionFragment.java
@@ -0,0 +1,18 @@
+package com.cmpt.memogram.ui.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.cmpt.memogram.R;
+
+public class CollectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_collection, container, false);
+    }
+}
